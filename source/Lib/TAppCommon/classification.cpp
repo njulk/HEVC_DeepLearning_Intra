5394e6a93aaed3f5975e71640d1eef200cd5bd05
njulk <425172524@qq.com>
2018-01-04 13:47:48 +0800

    modify classify

--- source/Lib/TAppCommon/classification.cpp
+++ source/Lib/TAppCommon/classification.cpp
@@ -4,6 +4,51 @@ static bool PairCompare(const std::pair<float, int>& lhs,
 	return lhs.first > rhs.first;
 }
 
+Classifier::Classifier(const string& model_file,
+	const string& trained_file,
+	const string& mean_file,
+	const string& label_file) {
+#ifdef CPU_ONLY
+	Caffe::set_mode(Caffe::CPU);
+#else
+	Caffe::set_mode(Caffe::GPU);
+#endif
+
+	/* Load the network. */
+	net_.reset(new Net<float>(model_file, TEST));
+	net_->CopyTrainedLayersFrom(trained_file);
+
+	CHECK_EQ(net_->num_inputs(), 1) << "Network should have exactly one input.";
+	CHECK_EQ(net_->num_outputs(), 1) << "Network should have exactly one output.";
+
+	Blob<float>* input_layer = net_->input_blobs()[0];
+	num_channels_ = input_layer->channels();
+	CHECK(num_channels_ == 3 || num_channels_ == 1)
+		<< "Input layer should have 1 or 3 channels.";
+	input_geometry_ = cv::Size(input_layer->width(), input_layer->height());
+
+	/* Load the binaryproto mean file. */
+	SetMean(mean_file);
+
+	/* Load labels. */
+	std::ifstream labels(label_file.c_str());
+	CHECK(labels) << "Unable to open labels file " << label_file;
+	string line;
+	while (std::getline(labels, line))
+		labels_.push_back(string(line));
+
+	Blob<float>* output_layer = net_->output_blobs()[0];
+	CHECK_EQ(labels_.size(), output_layer->channels())
+		<< "Number of labels is different from the output layer dimension.";
+}
+
+static bool PairCompare(const std::pair<float, int>& lhs,
+	const std::pair<float, int>& rhs) {
+	return lhs.first > rhs.first;
+}
+
+
+
 /* Return the indices of the top N values of vector v. */
 static std::vector<int> Argmax(const std::vector<float>& v, int N) {
 	std::vector<std::pair<float, int> > pairs;
